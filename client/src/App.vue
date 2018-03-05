24dd7006910dcebe4fdc66e9a8a83f9e4104ca0a
Bea Esguerra <bea.esguerra@hotmail.com>
2018-03-05 02:59:59 -0700

    setting up vue

---
+++ client/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>{{myMessage}}</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      myMessage: 'Hi there Bea',
+
+    };
+  },
+  methods: {
+    addTwo: a => a + 2,
+  }
+};
+</script>
+
+<style>
+</style>
