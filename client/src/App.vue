554e6324629df9f154c66214bcfc4f144f58dea0
Bea Esguerra <bea.esguerra@hotmail.com>
2018-03-07 02:20:31 -0700

    messenger working basically without users

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,31 +1,56 @@
 <template>
   <div id="app">
     <ul>
-      <li v-for="message in messages" :key="message">
-        {{message}}
+      <li v-for="message in messages" :key="message._id">
+        {{message.from}}
+        <br>
+        {{message.text}}
       </li>
     </ul>
-    <input>
-    <button> Send </button>
+    <input v-model="inputField" v-on:keyup.enter="send">
+    <button v-on:click="send" :disabled='inputIsEmpty'> Send </button>
   </div>
 </template>
 
 <script>
 
+import * as io from 'socket.io-client';
+import * as feathers from '@feathersjs/feathers';
+import * as socketio from '@feathersjs/socketio-client';
+
 export default {
   name: 'App',
+  async created() {
+    const socket = io('http://localhost:3030');
+    this.client = feathers();
+    this.client.configure(socketio(socket));
+    this.client.service('messages')
+      .on('created', message => this.messages.push(message));
+    this.messages = await this.client.service('messages').find({});
+  },
+  computed: {
+    inputIsEmpty() {
+      return this.inputField.length == 0
+    }
+  },
   data() {
     return {
       messages: [
-        'hi',
-        'hello',
-        'bye',
       ],
-
+      inputField: '',
+      client: null,
     };
   },
   methods: {
-    addTwo: a => a + 2,
+    send() {
+      if (this.inputField.length > 0) {
+        this.client.service('messages').create({
+          text: this.inputField,
+          from: 'Bea',
+        });
+        this.inputField = '';
+      }
+    },
   },
 };
 </script>
